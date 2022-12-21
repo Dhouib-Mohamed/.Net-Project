8d4b5b01d0a31b4f874bdb8c4f01429faf75be68
farah seddik <farahseddik500@gmail.com>
2022-12-21 15:13:49 +0100

    first

---
+++ project .net/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project_.net.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
