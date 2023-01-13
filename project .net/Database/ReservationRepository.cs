faf7f7e4f3490e988123d736a4503b76376ca565
dhouib <mohamed.dhouib@insat.ucar.tn>
2023-01-13 18:27:49 +0100

    changes

--- project .net/Database/ReservationRepository.cs
+++ project .net/Database/ReservationRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace project_.net.Database
+﻿using project_.net.Models;
+
+namespace project_.net.Database
 {
     public class ReservationRepository
     {
+        Context context = Context.getInstance();
+
+        public void addReservation(int clientId, int restaurantId, DateTime date, int nb)
+        {
+            ClientRepository clientRepository= new ClientRepository();
+            Client client = clientRepository.getClientById(clientId);
+            RestaurantRepository restaurantRepository = new RestaurantRepository();
+            Restaurant restaurant = restaurantRepository.getRestaurantById(restaurantId);
+            Reservation reservation = new Reservation(date, nb);
+            reservation.client = client;
+            reservation.restaurant = restaurant;
+            context.Reservation.Add(reservation);
+            context.SaveChanges();
+        }
     }
 }
