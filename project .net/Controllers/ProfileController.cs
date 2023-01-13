8fb26365b70bf47286103e91befa55fb1d7a22e6
farah seddik <farahseddik500@gmail.com>
2023-01-13 19:22:55 +0100

    uwu

--- project .net/Controllers/ProfileController.cs
+++ project .net/Controllers/ProfileController.cs
@@ -9,42 +9,38 @@ namespace project_.net.Controllers
         ClientRepository rep1 = new ClientRepository();
         public IActionResult ProfileDetails()
         {
+            ViewData["NavMenuPage"] = "profile";
             if (HttpContext.Session.GetInt32("user") != 1)
             {
                 return RedirectToAction("/Home/Signin");
             }
             else
             {
-                return View();
+                Client c = rep1.getClientById(1);
+                return View(c);
             }
         }
-            ViewData["NavMenuPage"] = "profile";
-            Client c = rep1.getClientById(1);
-            return View(c);
-        }
+           
+        
         ReservationRepository rep2 = new ReservationRepository();
         public IActionResult History()
         {
-            if (HttpContext.Session.GetInt32("user")!=1)
+            ViewData["NavMenuPage"] = "profile";
+        if (HttpContext.Session.GetInt32("user")!=1)
             {
                 return RedirectToAction("Signin","Home");
             }
             else
             {
-                return View();
+            Reservation r = rep2.getReservationByClientId(1);
+            return View(r);
             }
         }
 
-        public IActionResult myReservations()
-        {
-            return View();
-        }
 
     }
 }
-            Reservation r = rep2.getReservationByClientId(1);
-            ViewData["NavMenuPage"] = "profile";
-            return View(r);
+            
         }
         public IActionResult Edit()
         {
