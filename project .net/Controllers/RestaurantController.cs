8dc3f87faa874b8c0576fd6d28e317541e9ab73e
dhouib <mohamed.dhouib@insat.ucar.tn>
2023-01-14 10:33:29 +0100

    hi

--- project .net/Controllers/RestaurantController.cs
+++ project .net/Controllers/RestaurantController.cs
@@ -31,6 +31,7 @@ namespace project_.net.Controllers
             else
             {
                 ViewData["NavMenuPage"] = "connected";
+                ViewBag.search = collection["search"];
                 var restaurants = repository.RestaurantListByName(collection["search"]);
                 return View(restaurants);
             }
